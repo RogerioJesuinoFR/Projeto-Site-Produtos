13710366311374405472f572ca338cb8815494a9
Rogerio Jesuino Ferraz Rocha <139191290+RogerioJesuinoFR@users.noreply.github.com>
2024-05-13 11:11:53 -0300

    finalizado o projeto

--- pages/cadastro_usuario/pagina_de_listas.js
+++ pages/cadastro_usuario/pagina_de_listas.js
@@ -15,10 +15,17 @@ class ListManager {
             const listElement = document.createElement('li');
             listElement.id = list.id;
             listElement.innerHTML = `
+                <hr>
                 <input type="text" value="${list.name}" onchange="listManager.editListName('${list.id}', this.value)">
                 <button onclick="listManager.deleteList('${list.id}')">Excluir Lista</button>
                 <ul id="${list.id}-items">
-                    ${list.items.map(item => `<li>${item.name} <button onclick="listManager.deleteItem('${list.id}', '${item.id}')">Excluir</button></li>`).join('')}
+                    ${list.items.map(item => `
+                        <li>
+                            <span>${item.name}</span>
+                            <button onclick="listManager.editItem('${list.id}', '${item.id}', '${item.name}')">Editar</button>
+                            <button onclick="listManager.deleteItem('${list.id}', '${item.id}')">Excluir</button>
+                        </li>
+                    `).join('')}
                 </ul>
                 <input type="text" id="${list.id}-new-item" placeholder="Novo Item">
                 <button onclick="listManager.addItem('${list.id}')">Adicionar Item</button>
@@ -78,6 +85,19 @@ class ListManager {
         this.lists[listIndex].items.splice(itemIndex, 1);
         this.renderLists();
     }
+
+    editItem(listId, itemId, itemName) {
+        const newName = prompt("Novo nome do item:", itemName);
+        if (newName.trim() === '') {
+            alert ("Insira um nome.");
+        } else {
+            const listIndex = this.lists.findIndex(list => list.id === listId);
+            const itemIndex = this.lists[listIndex].items.findIndex(item => item.id === itemId);
+            this.lists[listIndex].items[itemIndex].name = newName;
+            this.renderLists();
+        }
+    }
+    
 }
 
 const listManager = new ListManager();
